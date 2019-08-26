f103514124f7c6b6911ebef5242b489e0e1412e9
Gorgeous-IMYun <392555078@qq.com>
2019-08-27 00:24:01 +0800

    尚硅谷_SpringBoot_web开发-错误处理原理&定制错误页面

--- src/main/java/com/atguigu/springboot04webrestfulcrud/controller/EmployeeController.java
+++ src/main/java/com/atguigu/springboot04webrestfulcrud/controller/EmployeeController.java
@@ -5,9 +5,11 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import com.atguigu.springboot04webrestfulcrud.dao.DepartmentDao;
 import com.atguigu.springboot04webrestfulcrud.dao.EmployeeDao;
@@ -49,20 +51,37 @@ public class EmployeeController {
 	public String addEmp(Employee employee) {
 		// 来到员工列表页面
 		System.out.println("保存员工的信息" + employee);
-		//保存员工
+		// 保存员工
 		employeeDao.save(employee);
 		// redirect:表示重定向到一个地址
 		// forward:表示转发到一个地址
 		return "redirect:/emps";
 	}
-	
-	//来到修改页面,查出当前员工，在页面回显
+
+	// 来到修改页面,查出当前员工，在页面回显
 	@GetMapping("/emp/{id}")
-	public String toEditPage(@PathVariable("id")Integer id,Model model) {
-	Employee employee=	employeeDao.get(id);
-		model.addAttribute("emp",employee);
-		//回到修改页面(add是一个修改添加二合一的页面)
-		System.err.print("employee-->"+employee);
+	public String toEditPage(@PathVariable("id") Integer id, Model model) {
+		Employee employee = employeeDao.get(id);
+		model.addAttribute("emp", employee);
+		// 页面要显示所有的部门列表
+		Collection<Department> departments = departmentDao.getDepartments();
+		model.addAttribute("depts", departments);
+		// 回到修改页面(add是一个修改添加二合一的页面)
 		return "emp/add";
+	}
+	
+	//员工修改:需要提交员工id
+	@PutMapping("/emp")
+	public String updateEmployee(Employee employee) {
+		System.out.println("修改的员工数据:"+employee);
+		employeeDao.save(employee);
+		return "redirect:/emps";
+	}
+	
+	//员工删除
+	@DeleteMapping("/emp/{id}")
+	public String deleteEmployee(@PathVariable("id") Integer id) {
+		employeeDao.delete(id);
+		return "redirect:/emps";
 	}
 }
